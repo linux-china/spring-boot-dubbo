63778272bf62a73f9e7d83d3519b6703745cdc63
linux_china <linux_china@hotmail.com>
2015-10-19 19:30:29 +0800

    portal controller

---
+++ spring-boot-dubbo-client/src/main/java/org/mvnsearch/PortalController.java
@@ -0,0 +1,24 @@
+package org.mvnsearch;
+
+import org.mvnsearch.uic.UicTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * portal controller
+ *
+ * @author linux_china
+ */
+@Controller
+public class PortalController {
+    @Autowired
+    private UicTemplate uicTemplate;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", uicTemplate.findById(1L));
+        return "index";
+    }
+}
