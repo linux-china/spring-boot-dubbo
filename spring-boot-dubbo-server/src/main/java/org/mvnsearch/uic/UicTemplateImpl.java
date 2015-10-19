25673b513db6f11043d5486dd945bc186ecc1212
linux_china <linux_china@hotmail.com>
2015-10-19 19:52:29 +0800

    uic template implementation

---
+++ spring-boot-dubbo-server/src/main/java/org/mvnsearch/uic/UicTemplateImpl.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.uic;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * uic template implementation
+ *
+ * @author linux_china
+ */
+@Component(value = "uicTemplate")
+public class UicTemplateImpl implements UicTemplate {
+    public User findById(Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setNick("nick:" + id);
+        user.setCreatedAt(new Date());
+        return user;
+    }
+}
