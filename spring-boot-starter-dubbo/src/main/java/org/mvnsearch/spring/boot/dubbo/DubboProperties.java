5e833b4e9414ce193ad1b01e9e7e4f04e20e9806
linux_china <linux_china@hotmail.com>
2015-10-28 10:31:20 +0800

    add threads and javadoc

--- spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboProperties.java
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboProperties.java
@@ -9,10 +9,26 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "spring.dubbo")
 public class DubboProperties {
+    /**
+     * dubbo application name
+     */
     private String app;
+    /**
+     * dubbo registry address
+     */
     private String registry;
+    /**
+     * communication protocol, default is dubbo
+     */
     private String protocol = "dubbo";
+    /**
+     * dubbo listen port, default 20800
+     */
     private Integer port = 20800;
+    /**
+     * dubbo thread count, default 200
+     */
+    private Integer threads = 200;
 
     public String getApp() {
         return app;
@@ -44,5 +60,13 @@ public class DubboProperties {
 
     public void setPort(Integer port) {
         this.port = port;
+    }
+
+    public Integer getThreads() {
+        return threads;
+    }
+
+    public void setThreads(Integer threads) {
+        this.threads = threads;
     }
 }
