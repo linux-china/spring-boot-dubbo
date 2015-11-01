acef1eeca33ce02495118bb0bb70dac7f0d0ec8f
linux_china <linux_china@hotmail.com>
2015-11-01 18:43:36 +0800

    add conditional

--- spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboAutoConfiguration.java
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class DubboAutoConfiguration {
     private DubboProperties properties;
 
     @Bean
+    @ConditionalOnMissingBean
     public ApplicationConfig dubboApplicationConfig() {
         ApplicationConfig appConfig = new ApplicationConfig();
         appConfig.setName(properties.getApp());
@@ -28,6 +30,7 @@ public class DubboAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public ProtocolConfig dubboProtocolConfig() {
         ProtocolConfig protocolConfig = new ProtocolConfig();
         protocolConfig.setName(properties.getProtocol());
@@ -37,6 +40,7 @@ public class DubboAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public RegistryConfig dubboRegistryConfig() {
         RegistryConfig registryConfig = new RegistryConfig();
         registryConfig.setAddress(properties.getRegistry());
