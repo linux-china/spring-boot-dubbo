18ce5a011ad715337251d6f6d1892926b4163508
linux_china <linux_china@hotmail.com>
2015-11-09 16:15:28 +0800

    consumer invoke statics filter

---
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/listener/ConsumerInvokeStaticsFilter.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.spring.boot.dubbo.listener;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * consumer invoke statics filter
+ *
+ * @author linux_china
+ */
+@Activate(group = Constants.CONSUMER, order = -110000)
+public class ConsumerInvokeStaticsFilter extends StaticsFilter {
+    public static Map<String, AtomicLong> statics = new ConcurrentHashMap<String, AtomicLong>();
+
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        increase(invocation.getClass(), invocation.getMethodName());
+        return invoker.invoke(invocation);
+    }
+}
