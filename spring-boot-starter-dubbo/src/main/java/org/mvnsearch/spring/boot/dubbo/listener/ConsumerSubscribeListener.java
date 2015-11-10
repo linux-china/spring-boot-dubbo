19276c3859e942d24c1610c9ad52ae31834e356a
linux_china <linux_china@hotmail.com>
2015-11-10 10:18:05 +0800

    添加连接信息

--- spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/listener/ConsumerSubscribeListener.java
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/listener/ConsumerSubscribeListener.java
@@ -1,12 +1,12 @@
 package org.mvnsearch.spring.boot.dubbo.listener;
 
+import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.listener.InvokerListenerAdapter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * dubbo client invoker listener
@@ -16,14 +16,26 @@ import java.util.Set;
 @Activate
 public class ConsumerSubscribeListener extends InvokerListenerAdapter {
     public static Set<Class> subscribedInterfaces = new HashSet<Class>();
+    public static Map<String, Set<String>> connections = new HashMap<String, Set<String>>();
 
     @Override
     public void referred(Invoker<?> invoker) throws RpcException {
-        subscribedInterfaces.add(invoker.getInterface());
+        Class<?> subscribeInterface = invoker.getInterface();
+        subscribedInterfaces.add(subscribeInterface);
+        String subscribeInterfaceCanonicalName = subscribeInterface.getCanonicalName();
+        if (!connections.containsKey(subscribeInterfaceCanonicalName)) {
+            connections.put(subscribeInterfaceCanonicalName, new HashSet<String>());
+        }
+        connections.get(subscribeInterfaceCanonicalName).add(invoker.getUrl().toString());
     }
 
     @Override
     public void destroyed(Invoker<?> invoker) {
-        subscribedInterfaces.remove(invoker.getInterface());
+        Class<?> subscribedInterface = invoker.getInterface();
+        subscribedInterfaces.remove(subscribedInterface);
+        String subscribedInterfaceCanonicalName = subscribedInterface.getCanonicalName();
+        if (connections.containsKey(subscribedInterfaceCanonicalName)) {
+            connections.get(subscribedInterfaceCanonicalName).remove(invoker.getUrl().toString());
+        }
     }
 }
