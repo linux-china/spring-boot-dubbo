ac87f896de4f86eadae6c817f2747099f3061ba3
linux_china <linux_china@hotmail.com>
2015-11-11 09:31:47 +0800

    minor update

--- spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboEndpoint.java
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboEndpoint.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
