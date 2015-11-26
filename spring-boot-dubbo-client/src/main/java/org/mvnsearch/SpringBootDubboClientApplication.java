0aa442449c76c33754851c4aed1fcbe33acaf289
linux_china <linux_china@hotmail.com>
2015-11-26 16:26:39 +0800

    remove reference bean

--- spring-boot-dubbo-client/src/main/java/org/mvnsearch/SpringBootDubboClientApplication.java
+++ spring-boot-dubbo-client/src/main/java/org/mvnsearch/SpringBootDubboClientApplication.java
@@ -1,26 +1,13 @@
 package org.mvnsearch;
 
-import com.alibaba.dubbo.config.spring.ReferenceBean;
-import org.mvnsearch.uic.UicTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("/uic-dubbo-consumer.xml")
 public class SpringBootDubboClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDubboClientApplication.class, args);
     }
-
-    /*@Bean
-    public ReferenceBean uicTemplate() {
-        ReferenceBean<UicTemplate> referenceBean = new ReferenceBean<UicTemplate>();
-        referenceBean.setInterface(UicTemplate.class);
-        referenceBean.setId("uicTemplate");
-        referenceBean.setTimeout(10000);
-        return referenceBean;
-    }*/
 }
