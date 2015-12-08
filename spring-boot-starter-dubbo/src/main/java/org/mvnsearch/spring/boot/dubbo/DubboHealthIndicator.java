6f73ad05de3703e8018ace3573ab4947e17ca9fa
linux_china <linux_china@hotmail.com>
2015-12-08 11:53:46 +0800

    extends AbstractHealthIndicator

--- spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboHealthIndicator.java
+++ spring-boot-starter-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboHealthIndicator.java
@@ -3,6 +3,7 @@ package org.mvnsearch.spring.boot.dubbo;
 import com.alibaba.dubbo.rpc.service.EchoService;
 import org.mvnsearch.spring.boot.dubbo.listener.ConsumerSubscribeListener;
 import org.springframework.beans.BeansException;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.ApplicationContext;
@@ -15,26 +16,22 @@ import org.springframework.stereotype.Component;
  * @author linux_china
  */
 @Component
-public class DubboHealthIndicator implements HealthIndicator, ApplicationContextAware {
+public class DubboHealthIndicator extends AbstractHealthIndicator implements ApplicationContextAware {
     private ApplicationContext applicationContext;
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
-    public Health health() {
-        Health.Builder builder = Health.up();
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
         if (!ConsumerSubscribeListener.subscribedInterfaces.isEmpty()) {
-            try {
-                for (Class clazz : ConsumerSubscribeListener.subscribedInterfaces) {
-                    EchoService echoService = (EchoService) applicationContext.getBean(clazz);
-                    echoService.$echo("Hello");
-                    builder.withDetail(clazz.getCanonicalName(), true);
-                }
-            } catch (Exception e) {
-                return Health.down().withDetail("Dubbo", e.getMessage()).build();
+            for (Class clazz : ConsumerSubscribeListener.subscribedInterfaces) {
+                EchoService echoService = (EchoService) applicationContext.getBean(clazz);
+                echoService.$echo("Hello");
+                builder.withDetail(clazz.getCanonicalName(), true);
             }
         }
-        return builder.build();
+        builder.up();
     }
+
 }
