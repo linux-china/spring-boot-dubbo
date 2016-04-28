e7a986c71fbcbbedb0cc6c84629161712703f804
雷卷 <jacky.chenlb@alibaba-inc.com>
2016-04-28 13:07:43 +0800

    dubbo consumer

---
+++ spring-boot-starter-dubbo/src/main/java/com/alibaba/dubbo/config/annotation/DubboConsumer.java
@@ -0,0 +1,15 @@
+package com.alibaba.dubbo.config.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * dubbo consumer
+ *
+ * @author linux_china
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface DubboConsumer {
+}
